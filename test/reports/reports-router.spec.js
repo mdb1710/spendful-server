20da2cd014d74a71b2efe3a3bc8c4077117540fc
Ethan Zimmerman <thebinarypenguin@gmail.com>
2019-04-24 17:56:31 -0400

    more report tests

--- test/reports/reports-router.spec.js
+++ test/reports/reports-router.spec.js
@@ -23,15 +23,42 @@ describe('GET /api/reports', () => {
 
   context('with invalid Authorization', () =>{
 
-    it.skip('should respond with an error (401)', () => {
-      expect(false).to.be.true();
+    it('should respond with an error (401)', () => {
+
+      return supertest(app)
+        .get('/api/reports')
+        .set('Authorization', 'Bearer INVALID_TOKEN')
+        .expect('Content-Type', /json/)
+        .expect(401)
+        .then(resp => {
+
+          const schema = joi.object({
+            errors: joi.array().required(),
+          });
+
+          joi.assert(resp.body, schema);
+        });
     });
   });
 
   context('with valid Authorization', () =>{
 
-    it.skip('should respond with a collection (object) of reports for each year (200)', () => {
-      expect(false).to.be.true();
+    it('should respond with a collection (object) of reports for each year (200)', () => {
+
+      return supertest(app)
+        .get('/api/reports')
+        .set('Authorization', `Bearer ${VALID_AUTH_TOKEN}`)
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .then(resp => {
+
+          // const schema = joi.object({
+          //   incomes  : joi.array().required(),
+          //   expenses : joi.array().required(),
+          // });
+
+          // joi.assert(resp.body, schema);
+        });
     });
   });
 });
@@ -40,22 +67,62 @@ describe('GET /api/reports/:year', () => {
 
   context('with invalid Authorization', () =>{
 
-    it.skip('should respond with an error (401)', () => {
-      expect(false).to.be.true();
+    it('should respond with an error (401)', () => {
+
+      return supertest(app)
+        .get('/api/reports/2019')
+        .set('Authorization', 'Bearer INVALID_TOKEN')
+        .expect('Content-Type', /json/)
+        .expect(401)
+        .then(resp => {
+
+          const schema = joi.object({
+            errors: joi.array().required(),
+          });
+
+          joi.assert(resp.body, schema);
+        });
     });
   });
 
   context('with invalid :year', () =>{
 
-    it.skip('should respond with an error (404)', () => {
-      expect(false).to.be.true();
+    it('should respond with an error (404)', () => {
+
+      return supertest(app)
+        .get('/api/reports/INVALID')
+        .set('Authorization', `Bearer ${VALID_AUTH_TOKEN}`)
+        .expect('Content-Type', /json/)
+        .expect(404)
+        .then(resp => {
+
+          const schema = joi.object({
+            errors: joi.array().required(),
+          });
+
+          joi.assert(resp.body, schema);
+        });
     });
   });
 
   context('with valid Authorization and :year', () =>{
 
-    it.skip('should respond with the report for the specified year (200)', () => {
-      expect(false).to.be.true();
+    it('should respond with the report for the specified year (200)', () => {
+
+      return supertest(app)
+        .get('/api/reports/2019')
+        .set('Authorization', `Bearer ${VALID_AUTH_TOKEN}`)
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .then(resp => {
+
+          // const schema = joi.object({
+          //   incomes  : joi.array().required(),
+          //   expenses : joi.array().required(),
+          // });
+
+          // joi.assert(resp.body, schema);
+        });
     });
   });
 });
