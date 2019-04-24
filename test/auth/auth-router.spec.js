32191bdce1e51bc1c027f6a53392e74312f18399
Ethan Zimmerman <thebinarypenguin@gmail.com>
2019-04-24 11:31:48 -0400

    Add tests for auth routes

--- test/auth/auth-router.spec.js
+++ test/auth/auth-router.spec.js
@@ -1,38 +1,89 @@
 /* global expect supertest*/
 'use strict';
 
+const joi = require('@hapi/joi');
 const app = require('../../src/app');
 
+// TODO MVP
 describe('POST /api/auth/login', () => {
 
   context('with invalid body (or credentials)', () =>{
 
     it.skip('should respond with an error (400)', () => {
-      expect(false).to.be.true();
+
+      return supertest(app)
+        .post('/api/auth/login')
+        .send({
+          email_address : 'INVALID',
+          password      : 'INVALID',
+        })
+        .expect('Content-Type', /json/)
+        .expect(400)
+        .then(resp => {
+          // TODO joi.assert(resp, someSchema);
+        });
     });
   });
 
   context('with valid body (and credentials)', () =>{
 
     it.skip('should respond with an auth token (200)', () => {
-      expect(false).to.be.true();
+
+      return supertest(app)
+        .post('/api/auth/login')
+        .send({
+          email_address : 'admin@spendful.com',
+          password      : 'password',
+        })
+        .expect('Content-Type', /json/)
+        .expect(400)
+        .then(resp => {
+
+          const schema = joi.object({
+            token: joi.string().required(),
+          });
+
+          joi.assert(resp.body, schema);
+        });
     });
   });
 });
 
+// TODO MVP
 describe('GET /api/auth/refresh', () => {
 
   context('with invalid Authorization', () =>{
 
     it.skip('should respond with an error (401)', () => {
-      expect(false).to.be.true();
+
+      return supertest(app)
+        .get('/api/auth/refresh')
+        .set('Authorization', 'Bearer INVALID_TOKEN')
+        .expect('Content-Type', /json/)
+        .expect(401)
+        .then(resp => {
+          // TODO joi.assert(resp, someSchema);
+        });
     });
   });
 
   context('with valid Authorization', () =>{
 
     it.skip('should respond with an auth token (200)', () => {
-      expect(false).to.be.true();
+
+      return supertest(app)
+        .get('/api/auth/refresh')
+        .set('Authorization', `Bearer ${VALID_AUTH_TOKEN}`)
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .then(resp => {
+
+          const schema = joi.object({
+            token: joi.string().required(),
+          });
+
+          joi.assert(resp.body, schema);
+        });
     });
   });
 });
--- test/setup.js
+++ test/setup.js
@@ -10,3 +10,5 @@ const supertest = require('supertest');
 
 global.expect = expect;
 global.supertest = supertest;
+
+global.VALID_AUTH_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJmdWxsX25hbWUiOiJTcGVuZGZ1bCBBZG1pbiIsImlhdCI6MTU1NjExODgyOH0.yJbhszxxQy1fQSvCwgdPLIuvnQQfK1elrJBQjdlYg6o';
