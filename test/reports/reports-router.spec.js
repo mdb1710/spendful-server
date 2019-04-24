1c8a7794e2151924ff0642307a6fe76c9b042cb6
Ethan Zimmerman <thebinarypenguin@gmail.com>
2019-04-24 17:34:55 -0400

    Add monthly report tests

--- test/reports/reports-router.spec.js
+++ test/reports/reports-router.spec.js
@@ -1,7 +1,23 @@
 /* global expect supertest*/
 'use strict';
-
+const joi = require('@hapi/joi');
+const knex = require('knex');
+const { PORT, DB_URL } = require('../../src/config');
 const app = require('../../src/app');
+
+before(() => {
+
+  const db = knex({
+    client: 'pg',
+    connection: DB_URL
+  });
+
+  app.set('db', db);
+});
+
+after(() => {
+  app.get('db').destroy();
+});
 
 describe('GET /api/reports', () => {
 
@@ -48,29 +64,82 @@ describe('GET /api/reports/:year/:month', () => {
 
   context('with invalid Authorization', () =>{
 
-    it.skip('should respond with an error (401)', () => {
-      expect(false).to.be.true();
+    it('should respond with an error (401)', () => {
+
+      return supertest(app)
+        .get('/api/reports/2019/4')
+        .set('Authorization', 'Bearer INVALID_TOKEN')
+        .expect('Content-Type', /json/)
+        .expect(401)
+        .then(resp => {
+
+          const schema = joi.object({
+            errors: joi.array().required(),
+          });
+
+          joi.assert(resp.body, schema);
+        });
     });
   });
 
   context('with invalid :year', () =>{
 
-    it.skip('should respond with an error (404)', () => {
-      expect(false).to.be.true();
+    it('should respond with an error (404)', () => {
+
+      return supertest(app)
+        .get('/api/reports/INVALID/4')
+        .set('Authorization', `Bearer ${VALID_AUTH_TOKEN}`)
+        .expect('Content-Type', /json/)
+        .expect(404)
+        .then(resp => {
+
+          const schema = joi.object({
+            errors: joi.array().required(),
+          });
+
+          joi.assert(resp.body, schema);
+        });
     });
   });
 
   context('with invalid :month', () =>{
 
-    it.skip('should respond with an error (404)', () => {
-      expect(false).to.be.true();
+    it('should respond with an error (404)', () => {
+
+      return supertest(app)
+        .get('/api/reports/2019/INVALID')
+        .set('Authorization', `Bearer ${VALID_AUTH_TOKEN}`)
+        .expect('Content-Type', /json/)
+        .expect(404)
+        .then(resp => {
+
+          const schema = joi.object({
+            errors: joi.array().required(),
+          });
+
+          joi.assert(resp.body, schema);
+        });
     });
   });
 
   context('with valid Authorization, :year, and :month', () =>{
 
-    it.skip('should respond with the report for the specified month (200)', () => {
-      expect(false).to.be.true();
+    it('should respond with the report for the specified month (200)', () => {
+
+      return supertest(app)
+        .get('/api/reports/2019/4')
+        .set('Authorization', `Bearer ${VALID_AUTH_TOKEN}`)
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .then(resp => {
+
+          const schema = joi.object({
+            incomes  : joi.array().required(),
+            expenses : joi.array().required(),
+          });
+
+          joi.assert(resp.body, schema);
+        });
     });
   });
 });
