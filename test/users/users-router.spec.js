a37063bd0e4834b1c4b80c3c808a30198bc547d1
Ethan Zimmerman <thebinarypenguin@gmail.com>
2019-04-24 13:03:56 -0400

    Add registration test

--- test/users/users-router.spec.js
+++ test/users/users-router.spec.js
@@ -1,7 +1,20 @@
 /* global expect supertest*/
 'use strict';
 
+const joi = require('@hapi/joi');
+const knex = require('knex');
+const { PORT, DB_URL } = require('../../src/config');
 const app = require('../../src/app');
+
+before(() => {
+
+  const db = knex({
+    client: 'pg',
+    connection: DB_URL
+  });
+
+  app.set('db', db);
+});
 
 describe('GET /api/users', () => {
 
@@ -35,8 +48,13 @@ describe('POST /api/users', () => {
     it.skip('should respond with an error (401)', () => {
 
       return supertest(app)
-        .get('/api/users')
+        .post('/api/users')
         .set('Authorization', 'Bearer INVALID_TOKEN')
+        .send({
+          full_name: 'John Doe',
+          email_address: 'jdoe@anon.com',
+          password: 'password',
+        })
         .expect('Content-Type', /json/)
         .expect(401)
         .then(resp => {
@@ -48,14 +66,39 @@ describe('POST /api/users', () => {
   context('with an invalid body', () =>{
 
     it.skip('should respond with an error (400)', () => {
-      expect(false).to.be.true();
+
+      return supertest(app)
+        .post('/api/users')
+        .set('Authorization', `Bearer ${VALID_AUTH_TOKEN}`)
+        .send({
+          foobar: 'invalid',
+        })
+        .expect('Content-Type', /json/)
+        .expect(400)
+        .then(resp => {
+          // TODO joi.assert(resp, someSchema);
+        });
     });
   });
 
   context('with valid Authorization and body', () =>{
 
     it.skip('should respond with a Location header and an empty body (201)', () => {
-      expect(false).to.be.true();
+
+      return supertest(app)
+        .post('/api/users')
+        .set('Authorization', `Bearer ${VALID_AUTH_TOKEN}`)
+        .send({
+          full_name: 'John Doe',
+          email_address: 'jdoe@anon.com',
+          password: 'password',
+        })
+        .expect('Content-Type', /json/)
+        .expect(201)
+        .expect('Authorization', /\/api\/users\//)
+        .then(resp => {
+          // TODO joi.assert(resp, someSchema);
+        });
     });
   });
 });
