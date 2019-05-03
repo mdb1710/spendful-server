a52bb1faee5a489215724f0054ce5e1a30bcde94
mdb1710 <mdb1710@gmail.com>
2019-05-03 15:29:38 -0400

    fixed eslint errors

--- src/app.js
+++ src/app.js
@@ -5,13 +5,13 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
-const { NODE_ENV } = require('./config')
-const authRouter = require('./auth/auth-route')
-const usersRouter = require('./users/users-router')
-const incomeRouter = require('./income/income-router')
-const expenseRouter = require('./expense/expense-route')
-const reportRouter = require('./reports/report-router')
-const categoryRouter = require('./categories/category-router')
+const { NODE_ENV } = require('./config');
+const authRouter = require('./auth/auth-route');
+const usersRouter = require('./users/users-router');
+const incomeRouter = require('./income/income-router');
+const expenseRouter = require('./expense/expense-route');
+const reportRouter = require('./reports/report-router');
+const categoryRouter = require('./categories/category-router');
 
 
 const app = express();
@@ -20,17 +20,17 @@ let morganOption = 'common';
 
 switch (NODE_ENV) {
 
-  case 'production':
-    morganOption = 'tiny';
-    break;
+case 'production':
+  morganOption = 'tiny';
+  break;
 
-  case 'development':
-    morganOption = 'common';
-    break;
+case 'development':
+  morganOption = 'common';
+  break;
 
-  case 'test':
-    morganOption = () => {};  // none
-    break;
+case 'test':
+  morganOption = () => {};  // none
+  break;
 }
 
 app.use(morgan(morganOption));
@@ -38,12 +38,12 @@ app.use(cors());
 app.use(helmet());
 
 
-app.use('/api/auth', authRouter)
-app.use('/api/users', usersRouter)
-app.use('/api/categories', categoryRouter)
-app.use('/api/incomes', incomeRouter)
-app.use('/api/expenses', expenseRouter)
-app.use('/api/reports', reportRouter)
+app.use('/api/auth', authRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/categories', categoryRouter);
+app.use('/api/incomes', incomeRouter);
+app.use('/api/expenses', expenseRouter);
+app.use('/api/reports', reportRouter);
 
 app.use(function clientErrorHandler(err, req, res, next) {
   if (req.xhr) {
