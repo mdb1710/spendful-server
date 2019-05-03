a056da08b8f406cbba1092319546dcb8e828594a
Ethan Zimmerman <thebinarypenguin@gmail.com>
2019-05-03 10:00:35 -0400

    Revert to simple delete logic (all cases)

--- src/income/income-service.js
+++ src/income/income-service.js
@@ -1,69 +1,46 @@
 
 
 const incomeService = {
-  getAllIncomes(db, owner_id){
-    return db('incomes')
-      .select('*')
-      .where({owner_id})
-  },
+    getAllIncomes(db, owner_id){
+        return db('incomes')
+            .select('*')
+            .where({owner_id})
+    },
 
-  getIncomeById(db, id, owner_id){
-    return db('incomes')
-      .select('*')
-      .where({id})
-      .andWhere({owner_id})
-      .first()
-    // .then(res => console.log(res))
-  },
-
-  insertIncome(db, income){
-    return db
-      .insert(income)
-      .into('incomes')
-      .returning('*')
-      .then(([income]) => income)
-    // .then(income => this.getIncomeById(db, income.id, income.owner_id))
-  },
-
-  deleteIncome(db, id){
-
-    return db('incomes')
-      .select('start_date, recurring_rule')
-      .where('id', id)
-      .first()
-      .then((row) => {
-
-        if (row.recurring_rule === null) {
-
-          // Delete
-          return db('incomes')
+    getIncomeById(db, id, owner_id){
+        return db('incomes')
+            .select('*')
             .where({id})
-            .delete();
+            .andWhere({owner_id})
+            .first()
+            // .then(res => console.log(res))
+    },
 
-        } else {
+    insertIncome(db, income){
+        return db
+            .insert(income)
+            .into('incomes')
+            .returning('*')
+            .then(([income]) => income)
+            // .then(income => this.getIncomeById(db, income.id, income.owner_id))
+    },
 
-          // Set end date
-
-          // FIXME
-          const lastOccurrence = new Date().toISOString();
-
-          return db('incomes')
+    deleteIncome(db, id){
+        return db('incomes')
             .where({id})
-            .update('end_date', lastOccurrence);
-        }
-      });
-  },
+            .delete()
+    },
 
-  updateIncome(db, updateData, id){
-    return db('incomes')
-      .where({id})
-      .update(updateData)
-      .returning('*')
-      .then(([income]) => income);
-  }
+    updateIncome(db, updateData, id){
+        return db('incomes')
+        .where({id})
+        .update(updateData)
+        .returning('*')
+        .then(([income]) => income)
+    }
 }
 
-module.exports = incomeService;
+module.exports = incomeService
 
 /**
  from expenses
